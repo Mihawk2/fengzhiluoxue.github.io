927e6ff72403b8de47563a8b50117989b2d30dde
yangzhou <2696289245@qq.com>
2019-06-27 15:48:33 +0800

    none

---
+++ src/components/myblog.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header">
+    <div class="head-message">欢迎来到风之落雪的个人博客</div>
+      <el-input
+        class="inputBar"
+        placeholder="请输入关键字"
+        suffix-icon="el-icon-search"
+        :model="input">
+      </el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'myblog',
+  data () {
+    return {
+      input:''
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .head-message{
+    width: 60%;
+    position: absolute;
+    font: 'Avenir', Helvetica, Arial, sans-serif;
+  }
+  .inputBar{
+    width: 40%;
+    float: right;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Myblog from '../components/myblog'
+import ElementUI from 'element-ui'
 
 Vue.use(Router)
+Vue.use(ElementUI)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'myblog',
+      component: Myblog
     }
   ]
 })
